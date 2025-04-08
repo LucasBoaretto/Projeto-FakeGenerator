ae408908a2152359871cd3832fd21d344e6860ea
Lucas Luiz Boaretto <lcboaretto@hotmail.com>
2025-04-08 17:54:44 -0300

    Inserido melhorias nas funções JavaScript para tornar elas mais genericas e tabem feito algumas alterações de estilo para deixar o site mais bonito

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,7 +33,7 @@ function contador(inputId, contId, maxId, maxValue) {
     cont.textContent = total;
 
     let color = 'green';
-    if (total >= maxValue){
+    if (total >= maxValue) {
         color = 'red';
     } else if (total >= maxValue - 15) {
         color = 'orange';
@@ -44,9 +44,9 @@ function contador(inputId, contId, maxId, maxValue) {
 }
 
 // Função que altera as letras minusculas para maiúsculas e vice-versa
-function toggleCase() {
-    let inserir = document.getElementById('insertText');
-    let mostrar = document.getElementById('textAreaHome');
+function toggleCase(inputId, outputId) {
+    let inserir = document.getElementById(inputId);
+    let mostrar = document.getElementById(outputId);
 
     let resultado = '';
     for (let i = 0; i < inserir.value.length; i++) {
@@ -62,9 +62,9 @@ function toggleCase() {
 }
 
 // Função que coloca a primeira letra de cada palavra do texto em maiuscula
-function properCase() {
-    let inserir = document.getElementById('insertText');
-    let mostrar = document.getElementById('textAreaHome');
+function properCase(inputId, outputId) {
+    let inserir = document.getElementById(inputId);
+    let mostrar = document.getElementById(outputId);
 
     let minusculo = inserir.value.toLowerCase();
     let separar = minusculo.split(' ');
@@ -81,9 +81,9 @@ function properCase() {
 
 
 // Função que coloca a primeira letra de cada frase em maiuscula
-function sentenceCase() {
-    let inserir = document.getElementById('insertText');
-    let mostrar = document.getElementById('textAreaHome');
+function sentenceCase(inputId, outputId) {
+    let inserir = document.getElementById(inputId);
+    let mostrar = document.getElementById(outputId);
 
     let minusculo = inserir.value.toLowerCase();
     let separar = minusculo.split(/([.!?])/);
@@ -99,9 +99,9 @@ function sentenceCase() {
 }
 
 // Função que coloca o texto em maiusculo
-function upperCase() {
-    let inserir = document.getElementById('insertText');
-    let mostrar = document.getElementById('textAreaHome');
+function upperCase(inputId, outputId) {
+    let inserir = document.getElementById(inputId);
+    let mostrar = document.getElementById(outputId);
 
     let resultado = '';
 
@@ -114,9 +114,9 @@ function upperCase() {
 }
 
 // Função que coloca o texto em minusculo
-function lowerCase() {
-    let inserir = document.getElementById('insertText');
-    let mostrar = document.getElementById('textAreaHome');
+function lowerCase(inputId, outputId) {
+    let inserir = document.getElementById(inputId);
+    let mostrar = document.getElementById(outputId);
 
     var resultado = '';
 
@@ -129,9 +129,9 @@ function lowerCase() {
 }
 
 // Função que coloca o texto em maiusculo e minusculo alternadamente
-function mixedCase() {
-    let inserir = document.getElementById('insertText');
-    let mostrar = document.getElementById('textAreaHome');
+function mixedCase(inputId, outputId) {
+    let inserir = document.getElementById(inputId);
+    let mostrar = document.getElementById(outputId);
 
     let resultado = '';
     let j = 0;
@@ -152,26 +152,21 @@ function mixedCase() {
 }
 
 // Função que apaga o texto da textarea
-function reset() {
-    var texto = document.getElementById('textAreaHome');
+function reset(outputId) {
+    var texto = document.getElementById(outputId);
     texto.value = '';
 }
 
 // Função que envia e valida os dados do formulário de cadastro
-function enviarCad() {
-    let nome = document.getElementById('nome');
-    let email = document.getElementById('email');
-    let telefone = document.getElementById('telefone');
-    let senha = document.getElementById('senha');
-    let verificar = document.getElementById('verificar');
-    const nameInput = document.getElementById('nome');
-    const emailInput = document.getElementById('email');
-    const phoneInput = document.getElementById('telefone');
-    const passwordInput = document.getElementById('senha');
-    const confirmPasswordInput = document.getElementById('verificar');
+function enviarCad(name, email, phone, password, confirmPassword) {
+    const nameInput = document.getElementById(name);
+    const emailInput = document.getElementById(email);
+    const phoneInput = document.getElementById(phone);
+    const passwordInput = document.getElementById(password);
+    const confirmPasswordInput = document.getElementById(confirmPassword);
 
     // confere se os campos obrigatórios estão preenchidos 
-    if (nome.value == '' && email.value == '' && senha.value == '' && verificar.value == '') {
+    if (nameInput.value == '' && emailInput.value == '' && passwordInput.value == '' && confirmPasswordInput.value == '') {
         Swal.fire({
             icon: "error",
             title: "ERRO",
@@ -181,75 +176,89 @@ function enviarCad() {
     } else { // incia as validações
 
         // confere se o nome nao possui numeros
-        for (let i = 0; i < nome.value.length; i++) {
-            if (!isNaN(nome.value[i])) {
+        for (let i = 0; i < nameInput.value.length; i++) {
+            if (!isNaN(nameInput.value[i])) {
                 Swal.fire({
                     icon: "error",
                     title: "ERRO",
                     text: "Nome inválido, insira um nome que não seja um número!"
                 });
                 nameInput.style.borderColor = 'red';
-                nome.value = '';
+                nameInput.value = '';
                 return;
             }
         }
 
         //confere se o nome tem mais de 3 letras
-        if (nome.value.length < 3) {
+        if (nameInput.value.length < 3) {
             Swal.fire({
                 icon: "error",
                 title: "ERRO",
                 text: "Nome inválido, insira um nome com no mínimo 3 letras!!"
             });
             nameInput.style.borderColor = 'red';
-            nome.value = '';
+            nameInput.value = '';
             return;
-
         }
+
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (!regex.test(emailInput.value)) {
+            Swal.fire({
+                icon: "error",
+                title: "ERRO",
+                text: "Email inválido, insira um email válido!"
+            });
+            emailInput.style.borderColor = 'red';
+            emailInput.value = '';
+            return;
+        }
+
+
         // confere se o telefone é uma letra
-        else if (isNaN(telefone.value)) {
+        if (isNaN(phoneInput.value)) {
             Swal.fire({
                 icon: "error",
                 title: "ERRO",
                 text: "Telefone inválido, insira apenas números"
             });
             phoneInput.style.borderColor = 'red';
-            telefone.value = '';
+            phoneInput.value = '';
             return;
         }
         //confere se o telefone tem mais de 10 digitos
-        else if (telefone.value.length > 0 && telefone.value.length < 10) {
+        if (phoneInput.value.length > 0 && phoneInput.value.length < 10) {
             Swal.fire({
                 icon: "error",
                 title: "ERRO",
                 text: "Telefone inválido, insira pelo menos 10 digitos!"
             });
             phoneInput.style.borderColor = 'red';
-            telefone.value = '';
+            phoneInput.value = '';
             return;
         }
 
         // confere se a senha tem mais de 6 digitos
-        else if (senha.value.length < 6) {
+        if (passwordInput.value.length < 6) {
             Swal.fire({
                 icon: "error",
                 title: "ERRO",
                 text: "Insira uma senha de no mínimo 6 digitos!"
             });
             passwordInput.style.borderColor = 'red';
-            senha.value = '';
+            passwordInput.value = '';
             return;
         }
 
         // confere se a confirmação da senha é igual a senha
-        else if (verificar.value !== senha.value) {
+        if (confirmPasswordInput.value !== passwordInput.value) {
             Swal.fire({
                 icon: "error",
                 title: "ERRO",
                 text: "As senhas não coincidem!"
             });
             confirmPasswordInput.style.borderColor = 'red';
-            verificar.value = '';
+            confirmPasswordInput.value = '';
             return;
         }
 
@@ -260,22 +269,27 @@ function enviarCad() {
                 title: "SUCESSO",
                 text: "Cadastro realizado com sucesso!"
             });
+            nameInput.style.borderColor = 'black';
+            emailInput.style.borderColor = 'black';
+            phoneInput.style.borderColor = 'black';
+            passwordInput.style.borderColor = 'black';
+            confirmPasswordInput.style.borderColor = 'black';
 
             const userData = {
-                nome: nome.value,
-                email: email.value,
-                telefone: telefone.value,
-                senha: senha.value
+                nome: nameInput.value,
+                email: emailInput.value,
+                telefone: phoneInput.value,
+                senha: passwordInput.value
             };
 
             const user = JSON.stringify(userData);
             console.log(user);
 
-            nome.value = '';
-            email.value = '';
-            telefone.value = '';
-            senha.value = '';
-            verificar.value = '';
+            nameInput.value = '';
+            emailInput.value = '';
+            phoneInput.value = '';
+            passwordInput.value = '';
+            confirmPasswordInput.value = '';
         }
     }
 }
