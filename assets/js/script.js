0c50e1b5e5028f966cc2d41a094e4462ad1f112b
Lucas Luiz Boaretto <lcboaretto@hotmail.com>
2025-04-10 15:19:28 -0300

    update função formulario

--- assets/js/script.js
+++ assets/js/script.js
@@ -155,6 +155,95 @@ function reset(outputId) {
     texto.value = '';
 }
 
+function showErrorMessage(message) {
+    Swal.fire({
+        icon: "error",
+        title: "ERRO",
+        text: message
+    });
+}
+
+function invalidStyle(input) {
+    input.style.borderColor = 'red';
+}
+
+function validStyle(input) {
+    input.style.borderColor = 'lightgreen';
+}
+
+function valName(input) {
+    // confere se o nome nao possui numeros
+    if (/\d/.test(input.value)) {
+        showErrorMessage('Nome inválido, insira apenas letras!');
+        invalidStyle(input);
+        return false;
+    }
+
+    //confere se o nome tem mais de 3 letras
+    if (input.value.length < 3) {
+        showErrorMessage('Nome inválido, insira um nome com no mínimo 3 letras!');
+        invalidStyle(input);
+        return false;
+    }
+    validStyle(input);
+    return true;
+}
+
+function valEmail(input) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!regex.test(input.value)) {
+        showErrorMessage('Insira um e-mail válido!');
+        invalidStyle(input);
+        return false;
+    }
+    validStyle(input);
+    return true;
+}
+
+function valPhone(input) {
+    // verifica se o campo está vazio
+    if (input.value.trim() === '') {
+        input.style.borderColor = 'black';
+        return true;
+    }
+
+    // confere se o telefone é uma letra
+    if (isNaN(input.value)) {
+        showErrorMessage('Telefone inválido, insira apenas números');
+        invalidStyle(input);
+        return false;
+    }
+
+    //confere se o telefone tem mais de 10 digitos
+    if (input.value.length > 0 && input.value.length < 10) {
+        showErrorMessage('Telefone inválido, insira pelo menos 10 digitos!');
+        invalidStyle(input);
+        return false;
+    }
+    validStyle(input);
+    return true;
+}
+
+function valPassword(password, confirm) {
+    // confere se a senha tem mais de 6 digitos
+    if (password.value.length < 6) {
+        showErrorMessage('Insira uma senha de no mínimo 6 digitos!');
+        invalidStyle(password);
+        return false;
+    }
+
+    // confere se a confirmação da senha é igual a senha
+    if (confirm.value !== password.value) {
+        showErrorMessage('As senhas não coincidem!');
+        invalidStyle(confirm);
+        return false;
+    }
+    validStyle(password);
+    validStyle(confirm);
+    return true;
+}
+
 // Função que envia e valida os dados do formulário de cadastro
 function enviarCad(name, email, phone, password, confirmPassword) {
     const nameInput = document.getElementById(name);
@@ -171,122 +260,40 @@ function enviarCad(name, email, phone, password, confirmPassword) {
             text: "Preencha todos os campos obrigatórios!"
         });
         return;
-    } else { // incia as validações
-
-        // confere se o nome nao possui numeros
-        if (/\d/.test(nameInput.value)) {
-            Swal.fire({
-                icon: "error",
-                title: "ERRO",
-                text: "Nome inválido, insira um nome que não seja um número!"
-            });
-            nameInput.style.borderColor = 'red';
-            nameInput.value = '';
-            return;
-        }
-
-
-        //confere se o nome tem mais de 3 letras
-        if (nameInput.value.length < 3) {
-            Swal.fire({
-                icon: "error",
-                title: "ERRO",
-                text: "Nome inválido, insira um nome com no mínimo 3 letras!!"
-            });
-            nameInput.style.borderColor = 'red';
-            nameInput.value = '';
-            return;
-        }
-
-        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-        if (!regex.test(emailInput.value)) {
-            Swal.fire({
-                icon: "error",
-                title: "ERRO",
-                text: "Email inválido, insira um email válido!"
-            });
-            emailInput.style.borderColor = 'red';
-            emailInput.value = '';
-            return;
-        }
-
-
-        // confere se o telefone é uma letra
-        if (isNaN(phoneInput.value)) {
-            Swal.fire({
-                icon: "error",
-                title: "ERRO",
-                text: "Telefone inválido, insira apenas números"
-            });
-            phoneInput.style.borderColor = 'red';
-            phoneInput.value = '';
-            return;
-        }
-        //confere se o telefone tem mais de 10 digitos
-        if (phoneInput.value.length > 0 && phoneInput.value.length < 10) {
-            Swal.fire({
-                icon: "error",
-                title: "ERRO",
-                text: "Telefone inválido, insira pelo menos 10 digitos!"
-            });
-            phoneInput.style.borderColor = 'red';
-            phoneInput.value = '';
-            return;
-        }
-
-        // confere se a senha tem mais de 6 digitos
-        if (passwordInput.value.length < 6) {
-            Swal.fire({
-                icon: "error",
-                title: "ERRO",
-                text: "Insira uma senha de no mínimo 6 digitos!"
-            });
-            passwordInput.style.borderColor = 'red';
-            passwordInput.value = '';
-            return;
-        }
-
-        // confere se a confirmação da senha é igual a senha
-        if (confirmPasswordInput.value !== passwordInput.value) {
-            Swal.fire({
-                icon: "error",
-                title: "ERRO",
-                text: "As senhas não coincidem!"
-            });
-            confirmPasswordInput.style.borderColor = 'red';
-            confirmPasswordInput.value = '';
-            return;
-        }
-
-        // "envia" o formulário se tudo estiver correto e cria um JSON com os dados do usuário
-        else {
-            Swal.fire({
-                icon: "success",
-                title: "SUCESSO",
-                text: "Cadastro realizado com sucesso!"
-            });
-            nameInput.style.borderColor = 'black';
-            emailInput.style.borderColor = 'black';
-            phoneInput.style.borderColor = 'black';
-            passwordInput.style.borderColor = 'black';
-            confirmPasswordInput.style.borderColor = 'black';
-
-            const userData = {
-                nome: nameInput.value,
-                email: emailInput.value,
-                telefone: phoneInput.value,
-                senha: passwordInput.value
-            };
-
-            const user = JSON.stringify(userData);
-            console.log(user);
-
-            nameInput.value = '';
-            emailInput.value = '';
-            phoneInput.value = '';
-            passwordInput.value = '';
-            confirmPasswordInput.value = '';
-        }
     }
+
+    if (!valName(nameInput)) return;
+    if (!valEmail(emailInput)) return;
+    if (!valPhone(phoneInput)) return;
+    if (!valPassword(passwordInput, confirmPasswordInput)) return;
+
+    // "envia" o formulário se tudo estiver correto e cria um JSON com os dados do usuário
+
+    Swal.fire({
+        icon: "success",
+        title: "SUCESSO",
+        text: "Cadastro realizado com sucesso!"
+    });
+    nameInput.style.borderColor = 'black';
+    emailInput.style.borderColor = 'black';
+    phoneInput.style.borderColor = 'black';
+    passwordInput.style.borderColor = 'black';
+    confirmPasswordInput.style.borderColor = 'black';
+
+    const userData = {
+        nome: nameInput.value,
+        email: emailInput.value,
+        telefone: phoneInput.value,
+        senha: passwordInput.value
+    };
+
+    const user = JSON.stringify(userData);
+    console.log(user);
+
+    nameInput.value = '';
+    emailInput.value = '';
+    phoneInput.value = '';
+    passwordInput.value = '';
+    confirmPasswordInput.value = '';
+
 }
